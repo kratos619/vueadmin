e06ebe15435ae7b9a1576763dec0d819ab910b99
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-26 23:34:20 +0530

    create new component user and add links to master pages

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <div class="row mt-3">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Responsive Hover Table</h3>
+
+            <div class="card-tools">
+              <span>
+                <button
+                  data-toggle="modal"
+                  data-target="#exampleModal"
+                  class="btn btn-primary btn-outline"
+                >
+                  <i class="fas fa-plus-circle"></i> Add New
+                </button>
+              </span>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body table-responsive p-0">
+            <table class="table table-hover">
+              <tbody>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Type</th>
+                  <th></th>
+                  <th>Modify</th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <td>183</td>
+                  <td>John Doe</td>
+                  <td>11-7-2014</td>
+                  <td>
+                    <span class="tag tag-success">Approved</span>
+                  </td>
+                  <td>
+                    <a class="btn btn-warning btn-sm btn-block" href>
+                      <i class="fas fa-pencil-alt"></i> Edit
+                    </a>
+                  </td>
+                  <td></td>
+                  <td>
+                    <a class="btn btn-danger btn-sm btn-block" href>
+                      <i class="fas fa-trash-alt"></i> Delete
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+    </div>
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
